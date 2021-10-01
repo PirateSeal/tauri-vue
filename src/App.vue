2c377b829747bdffb7d5844081d114974155447b
Thomas Cousin <tcousin.pseal@gmail.com>
2021-10-01 15:07:01 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Directory/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Directory from './components/Directory'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Directory
   }
 }
 </script>
---
+++ src/components/Directory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Directory component</h1>
+  </div>
+</template>
+
+<script>
+
+import {read_dir} from "@tauri-apps/api"
+import {onMounted} from "vue";
+
+export default {
+  name: "Directory",
+  setup() {
+    onMounted(async () => {
+      console.log('mounted!');
+      this.dirList = await read_dir("", {dir: 11})
+    })
+  },
+  data() {
+    return {
+      dirList: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
